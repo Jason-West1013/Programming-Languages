8f23c3ee00e455b5f87d1d90e62a41010f5401c9
Jason West <jasonwest1013@yahoo.com>
2019-01-30 09:48:40 -0500

    Fixed mac only bugs

--- C Project/main.c
+++ C Project/main.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-#include<string.h>
-#include<ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
 
-struct wordList {
-    char                *word;
-    int                 wordFreq;
-    struct wordList     *nextWord;
-    struct wordList     *prevWord;
+struct wordList
+{
+    char *word;
+    int wordFreq;
+    struct wordList *nextWord;
+    struct wordList *prevWord;
 };
 typedef struct wordList LIST;
 
@@ -17,27 +18,32 @@ LIST *HEAD;
 LIST *TAIL;
 FILE *FP;
 
-char *getWord() {
+char *getWord()
+{
     char c;
     int len = 0;
     char *word = (char *)malloc(sizeof(char));
 
     // Test for allocation
-    if (word == NULL) {
+    if (word == NULL)
+    {
         printf("Could not allocate memory.\n");
         exit(1);
     }
 
     // Check if end of file
-    if ((c = getc(FP)) == EOF) {
+    if ((c = getc(FP)) == EOF)
+    {
         return NULL;
     }
 
     // Retrieve word from file
-    while (isalpha(c) || isdigit(c) || c == 95 || c == 45) {
+    while (isalpha(c) || isdigit(c) || c == 95 || c == 45)
+    {
 
         // Test first letter
-        if (len == 0 && !isalpha(c)) {
+        if (len == 0 && !isalpha(c))
+        {
             break;
         }
 
@@ -52,15 +58,17 @@ char *getWord() {
     return word;
 }
 
-    // Read the file (1st pass) and create a linked list of words (in order) with freq set to 1
-int createList() {
+// Read the file (1st pass) and create a linked list of words (in order) with freq set to 1
+void createList()
+{
     LIST *listPtr;
     LIST *newElement;
     char *tempWord;
     bool found;
 
     // Add first word to the Linked List
-    if (tempWord = getWord()) {
+    if ((tempWord = getWord()))
+    {
         newElement = (LIST *)malloc(sizeof(LIST));
         newElement->word = tempWord;
         newElement->wordFreq = 1;
@@ -70,24 +78,30 @@ int createList() {
     }
 
     // Loop through the remainder of the file adding words
-    while (tempWord = getWord()) {
-        if (tempWord[0] != '\0') {
+    while ((tempWord = getWord()))
+    {
+        if (tempWord[0] != '\0')
+        {
             //printf("%s\n", tempWord);
             listPtr = HEAD;
             found = false;
-            if (listPtr->prevWord == NULL && strcmp(listPtr->word, tempWord) == 0) {
+            if (listPtr->prevWord == NULL && strcmp(listPtr->word, tempWord) == 0)
+            {
                 found = true;
             }
-            while (listPtr->nextWord != NULL && !found) {
+            while (listPtr->nextWord != NULL && !found)
+            {
                 listPtr = listPtr->nextWord;
-                if (strcmp(listPtr->word, tempWord) == 0) {
+                if (strcmp(listPtr->word, tempWord) == 0)
+                {
                     found = true;
                     break;
                 }
             }
 
             // add to linked list
-            if (!found) {
+            if (!found)
+            {
                 newElement = (LIST *)malloc(sizeof(LIST));
                 newElement->word = tempWord;
                 newElement->wordFreq = 1;
@@ -95,7 +109,9 @@ int createList() {
                 newElement->prevWord = listPtr;
                 listPtr->nextWord = newElement;
                 TAIL = newElement;
-            } else {
+            }
+            else
+            {
                 listPtr->wordFreq++;
             }
         }
@@ -103,36 +119,44 @@ int createList() {
     free(tempWord);
 }
 
-void printList() {
+void printList()
+{
     LIST *ptr = HEAD;
-    while (ptr != NULL) {
+    while (ptr != NULL)
+    {
         printf("Word: %-40s Freq: %d\n", ptr->word, ptr->wordFreq);
         ptr = ptr->nextWord;
     }
 }
 
-void alphaSortandPrint() {
-    // TODO: Sort the linked like alphabetically then invoke PrintList 
+void alphaSortandPrint()
+{
+    // TODO: Sort the linked like alphabetically then invoke PrintList
 }
 
-void freqSortandPrint() {
+void freqSortandPrint()
+{
     // TODO: Sort the linked list in decreasing order of word frequency and invoke PrintList
 }
 
-void freeList() {
+void freeList()
+{
     LIST *temp;
-    while(HEAD != NULL) {
+    while (HEAD != NULL)
+    {
         temp = HEAD;
         HEAD = HEAD->nextWord;
         free(temp);
     }
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     // Loop for all possible file inputs
     FP = fopen(argv[1], "r");
 
-    if (FP == NULL) {
+    if (FP == NULL)
+    {
         printf("Could not open file.\n");
         exit(0);
     }

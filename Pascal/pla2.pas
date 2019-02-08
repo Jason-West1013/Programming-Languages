c351a1d1ab0aeb57deb1b0054f27887839257912
Jason-West1013 <jasonwest1013@yahoo.com>
2019-02-08 00:47:38 -0500

    pascal: word count is complete

--- Pascal/pla2.pas
+++ Pascal/pla2.pas
@@ -1,48 +1,75 @@
 
 Program pla2;
 
-Const 
-  MAX_LINE_LENGTH = 100;
-
 Var 
   inputFile:      text;
-  letter:         char;
   i:              integer;
+  isWord:         boolean;
   totalLines:     integer;
   totalChars:     integer;
   totalWords:     integer;
-
-  line:           packed array [1..MAX_LINE_LENGTH] Of char;
+  line:           packed array [1..75] Of char;
+  word:           string;
 
 Begin
-  totalChars := 0;
   totalLines := 0;
+  totalChars := 0;
   totalWords := 0;
+  word := '';
 
   assign(inputFile, 'test4.txt');
   reset(inputFile);
 
-  // Reads in a line at a time
   While Not EOF (inputFile) Do
     Begin
+      isWord := true;
       readln(inputFile, line);
-
-      // counts total lines
       totalLines := totalLines + 1;
 
-      // loops through to pull out words and count chars/words
-      For i := 1 To MAX_LINE_LENGTH Do
+      For i := 1 To 75 Do
         Begin
           If (line[i] <> ' ') And (ord(line[i]) <> 0) Then
             totalChars := totalChars + 1;
 
+          // if it's a new word
+          If (word = '') Then
+            Begin
+              // test that the first character is a letter
+              If (line[i] In ['a'..'z', 'A'..'Z']) Then
+                Begin
+                  word := line[i];
+                  continue;
+                End
+              Else
+                // if not set flag, and if not a blank space continue
+                isWord := false;
+              If (line[i] <> ' ') And (ord(line[i]) <> 0) Then
+                Begin
+                  word := line[i];
+                  continue;
+                End;
+            End
+          Else
+            Begin
+              // if not a new word, test remaining characters
+              If (line[i] In ['a'..'z', 'A'..'Z', '0'..'9', '-', '_']) Then
+                Insert(line[i], word, length(word) + 1)
+              Else
+                Begin
+                  If (isWord = true) Then
+                    Begin
+                      // write word into list
+                      totalWords := totalWords + 1;
+                      //writeln(word);
+                    End;
+                  word := '';
+                  isWord := true;
+                End;
+            End;
         End;
-
     End;
 
-  writeln(totalChars);
   writeln(totalLines);
+  writeln(totalChars);
   writeln(totalWords);
-  writeln(line);
-  close(inputFile);
 End.

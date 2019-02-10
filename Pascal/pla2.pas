68a77929583dfab305b10ca1fe5f7b476d1e5497
Jason-West1013 <jasonwest1013@yahoo.com>
2019-02-10 00:34:24 -0500

    word frequency list done, gross but works.

--- Pascal/pla2.pas
+++ Pascal/pla2.pas
@@ -1,31 +1,46 @@
 
 Program pla2;
 
+// Record used to store the words and frequencies
+
+Type 
+  WordFreq = Record
+    word:         string;
+    freq:         integer;
+  End;
+
 Var 
   inputFile:      text;
   i:              integer;
+  j:              integer;
   isWord:         boolean;
+  wordFound:      boolean;
   totalLines:     integer;
   totalChars:     integer;
   totalWords:     integer;
   line:           packed array [1..75] Of char;
+  list:           packed array [1..200] Of WordFreq;
   word:           string;
 
 Begin
+  // Variable initiation
   totalLines := 0;
   totalChars := 0;
   totalWords := 0;
   word := '';
 
+  // Open file
   assign(inputFile, 'test4.txt');
   reset(inputFile);
 
+  // Begin of huge main loop 
   While Not EOF (inputFile) Do
     Begin
       isWord := true;
       readln(inputFile, line);
       totalLines := totalLines + 1;
 
+   // Put each line of the text file into an array and loop picking out the data
       For i := 1 To 75 Do
         Begin
           If (line[i] <> ' ') And (ord(line[i]) <> 0) Then
@@ -58,15 +73,48 @@ Begin
                 Begin
                   If (isWord = true) Then
                     Begin
-                      // write word into list
                       totalWords := totalWords + 1;
-                      //writeln(word);
+
+                      // Add word to list or increment its frequency
+                      j := 1;
+                      wordFound := false;
+
+                      // Search list for word ignoring empty elements
+                      While (ord(list[j].freq) <> 0) Do
+                        Begin
+                          If (list[j].word = word) Then
+                            Begin
+                              wordFound := true;
+                              break;
+                            End;
+                          j := j + 1;
+                        End;
+
+                      If Not(wordFound) Then
+                        Begin
+                          list[j].word := word;
+                          list[j].freq := 1;
+                        End
+                      Else
+                        Begin
+                          list[j].freq := list[j].freq + 1;
+                        End;
                     End;
+
+                  // reset values
                   word := '';
                   isWord := true;
                 End;
             End;
         End;
+    End;
+
+  i := 1;
+  While (ord(list[i].freq) <> 0) Do
+    Begin
+      writeln(list[i].word);
+      writeln(list[i].freq);
+      i := i + 1;
     End;
 
   writeln(totalLines);
